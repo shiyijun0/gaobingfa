e36632528b34e35513e1fc9c79ef9baa50da9dc0
shiyijun0 <617318842@qq.com>
2018-12-18 16:51:48 +0800

    提交插入数据库数据

--- src/test/java/cn/bdqn/gaobingfa/DataSource.java
+++ src/test/java/cn/bdqn/gaobingfa/DataSource.java
@@ -38,6 +38,7 @@ String str="INSERT INTO `product_info` VALUES";
                     PreparedStatement prp=connection.prepareStatement(str);
                     prp.execute();
                     str="INSERT INTO `product_info` VALUES";
+                    connection.commit();
                 } catch (SQLException e) {
                     e.printStackTrace();
                 }
@@ -45,5 +46,42 @@ String str="INSERT INTO `product_info` VALUES";
             }
 
                 });
+        bufferedReader.close();
+        connection.close();
+
+        LocalDateTime localDateTime1=LocalDateTime.now();
+        System.out.println("****1****"+localDateTime1);
+    }
+
+    @Test
+    public void test() throws Exception{
+        BufferedReader bufferedReader=new BufferedReader(new FileReader("D:\\product_info.sql"));
+        Connection connection=DriverManager.getConnection(url,user,password);
+        LocalDateTime localDateTime=LocalDateTime.now();
+        System.out.println("********"+localDateTime);
+        connection.setAutoCommit(false);
+        bufferedReader.lines().forEach(sql->{
+
+                try {
+                    PreparedStatement prp=connection.prepareStatement(sql);
+                    prp.addBatch();
+                    if((i%2000)!=0 && i<2097152){
+                        i++;
+                    }else {
+                        prp.executeBatch();
+                        connection.commit();
+                        i=0;
+                    }
+
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+
+        });
+        bufferedReader.close();
+        connection.close();
+
+        LocalDateTime localDateTime1=LocalDateTime.now();
+        System.out.println("****1****"+localDateTime1);
     }
 }
